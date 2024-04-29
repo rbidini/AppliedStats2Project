d6902d4594a1593e4476ad8c2a4f1355cc015024
rbidini <93054336+rbidini@users.noreply.github.com>
2024-04-28 22:01:44 -0400

    updated the cleaning file

--- cleaningdata.R
+++ cleaningdata.R
@@ -1,1 +1,17 @@
-test2
+library(readr)
+library(dplyr)
+library(forcats)
+cars_data <- read.csv("vehicles.csv")
+
+
+clean_cars <- cars_data %>% subset(select = -c(id,url,region_url,VIN,image_url,description,county,lat,long,posting_date))
+
+
+write.csv(clean_cars,"cleaned_cars.csv")
+
+
+
+
+cleaner_cars<- read.csv("cleaned_cars.csv")
+
+cleaner_cars2 <- cleaner_cars %>% subset(select = -c(X)) %>% filter(!is.na(odometer)|!is.na(year))
